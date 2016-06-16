67f21451c4024773a739852e1459a79538e950d5
Dallan Bhatti <dbhatti@sfu.ca>
2016-06-16 00:55:44 +0000

    some security features added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-
+before_action :logged_in_user, only: [:edit, :update, :index, :show]
+before_action :correct_user,   only: [:edit, :update]
 
   def new
     @user = User.new
@@ -28,12 +29,18 @@ class UsersController < ApplicationController
 
 
   def edit
-
+    @user = User.find(params[:id])
   end
 
 
   def update
-
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   def destroy
@@ -46,5 +53,16 @@ class UsersController < ApplicationController
     end
     
 
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+  end
 end
 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,10 +10,15 @@ module SessionsHelper
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+  
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
   end
+  
   
   def log_out
     session.delete(:user_id)
