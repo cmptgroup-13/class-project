5225d433fc066d9fb897faa50873f3431e49e76e
Dallan Bhatti <dbhatti@sfu.ca>
2016-06-20 20:03:12 +0000

    favicon

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,16 +2,7 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
-  test "invalid signup information" do
-    get signup_path
-    assert_no_difference 'User.count' do
-      post users_path, user: { name:  "",
-                               email: "user@invalid",
-                               password:              "foo",
-                               password_confirmation: "bar" }
-    end
-    assert_template 'users/new'
-  end
+  
   
   test "valid signup information" do
     get signup_path
@@ -23,5 +14,65 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             role: "male"}
     end
     assert_template 'users/show'
+  end
+  
+  test "invalid signup information no role" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, user: { name:  "1Example User",
+                                            email: "user1@example.com",
+                                            password:              "password",
+                                            password_confirmation: "password",
+                                            role: ""}
+    end
+    assert_template 'users/new'
+  end
+  
+  test "invalid signup information no password" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, user: { name:  "1Example User",
+                                            email: "user1@example.com",
+                                            password:              "",
+                                            password_confirmation: "",
+                                            role: "male"}
+    end
+    assert_template 'users/new'
+  end
+  
+  test "invalid signup information mismatching password and password confirm" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, user: { name:  "1Example User",
+                                            email: "user1@example.com",
+                                            password:              "password",
+                                            password_confirmation: "passord",
+                                            role: "male"}
+    end
+    assert_template 'users/new'
+  end
+  
+  test "invalid signup information no email" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, user: { name:  "1Example User",
+                                            email: "user1@e",
+                                            password:              "password",
+                                            password_confirmation: "password",
+                                            role: "male"}
+    end
+    assert_template 'users/new'
+  end
+
+  test "invalid signup information no name" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, user: { name:  "",
+                                            email: "user1@email.com",
+                                            password:              "password",
+                                            password_confirmation: "password",
+                                            role: "male"}
+    end
+    assert_template 'users/new'
   end
 end
