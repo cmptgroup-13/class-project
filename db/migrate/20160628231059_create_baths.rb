5ba9631105ba26f287b0ec547f7ef195f4373e94
Dallan Bhatti <dbhatti@sfu.ca>
2016-07-01 19:27:01 +0000

    added geo location to bathroom addresses so longitude and latitude can be used to find certain locations on the map

--- db/migrate/20160628231059_create_baths.rb
+++ db/migrate/20160628231059_create_baths.rb
@@ -9,6 +9,7 @@ class CreateBaths < ActiveRecord::Migration
       t.integer :rating
       t.references :user, foreign_key: true
 
+
       t.timestamps null: false
     end
     add_index :baths, [:user_id, :created_at]
