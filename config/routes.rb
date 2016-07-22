6b345680219340af030d2687a0f54f03b248e646
Dallan Bhatti <dbhatti@sfu.ca>
2016-07-22 20:13:38 +0000

    omniauth try 4

--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,7 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { registrations: "registrations", omniauth_callbacks: 'omniauth_callbacks' }
   match 'users/:id' => 'main#destroy', :via => :delete, :as => :admin_destroy_user
-  # match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
+  match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
  
 
   get 'change', to: 'registrations#change'
