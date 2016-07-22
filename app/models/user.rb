ba7259ff1601386f0ec98d5c19a88a8fb4a9a46b
Dallan Bhatti <dbhatti@sfu.ca>
2016-07-22 22:01:04 +0000

    1212121212

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,7 @@ class User < ActiveRecord::Base
     where(auth.slice(:provider, :uid)).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
-      user.name = auth.info.nickname
+      # user.name = auth.info.nickname
     end
   end
   
