84747b76622a849f2b785121308a514441d058ca
Renko-Mei <wxh960302@gmail.com>
2016-07-31 02:49:15 +0000

    test bathroom

--- spec/features/admin_bathroom_add_spec.rb
+++ spec/features/admin_bathroom_add_spec.rb
@@ -15,6 +15,8 @@ RSpec.feature "new bathroom" do
         fill_in "Address", with: "1825 Riverside Drive"
         click_on("Create my bathroom")
         expect(page).to have_content("All bathrooms")
+        visit 'showmap'
+        expect(page).to have_content("North Vancouver, 1825 Riverside Drive")
     end
     
     scenario "missing city" do
@@ -43,5 +45,27 @@ RSpec.feature "new bathroom" do
         expect(page).to have_content("Address can't be blank")
     end
     
-       
+    scenario "add bathroom to existed address" do
+        user = create(:user)
+        visit 'users/sign_in'
+        fill_in "Email", with: user.email
+        fill_in "Password", with: user.password
+        click_on("Log in")
+        visit 'createbath'
+        expect(page).to have_content("Please fill in all required fields to add your bathroom")
+        select 'Canada', from: "Country"
+        select 'British Columbia', from: "Province/State"
+        fill_in "City", with: "North Vancouver"
+        fill_in "Address", with: "1825 Riverside Drive"
+        click_on("Create my bathroom")
+        expect(page).to have_content("All bathrooms")
+        visit '/createbath'
+        expect(page).to have_content("Please fill in all required fields to add your bathroom")
+        select 'Canada', from: "Country"
+        select 'British Columbia', from: "Province/State"
+        fill_in "City", with: "North Vancouver"
+        fill_in "Address", with: "1825 Riverside Drive"
+        click_on("Create my bathroom")
+        expect(page).to have_content("Longitude and latitude data show this location is already taken")
+    end
 end
