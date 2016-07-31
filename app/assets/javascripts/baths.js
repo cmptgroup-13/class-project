d9750b485cfe534eec0117327dbc45dfaf001308
Dallan Bhatti <dbhatti@sfu.ca>
2016-07-31 10:44:30 +0000

    assets for heroku

--- app/assets/javascripts/baths.js
+++ app/assets/javascripts/baths.js
@@ -8,27 +8,3 @@ function nextSlide(){
 	slides[currentSlide].className = 'slide showing';
 }
 
-//showsingle below
-  myLatLng = new google.maps.LatLng({lat: <%= @bath.latitude %>, lng: <%= @bath.longitude %>});
-
-handler = Gmaps.build('Google');
-handler.buildMap({ provider: {zoom: 16, center: myLatLng}, internal: {id: 'map'}}, function(){
-  markers = handler.addMarkers([
-      {
-          "lat": <%= @bath.latitude %>,
-          "lng": <%= @bath.longitude %>,
-          "picture": {
-                    "url": "http://megaicons.net/static/img/icons_sizes/105/244/32/toilet-paper-icon.png",
-                     "width":  32,
-                     "height": 32
-
-          },
-          "infowindow": "hello!"
-      }
-      ]);
-
-
-
-  handler.bounds.extendWith(markers);
-  //handler.fitMapToBounds();
-});
