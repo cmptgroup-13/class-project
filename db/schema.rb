3abc074e802ccc0a7ad3ec78d661c36c9b75b6e1
Dallan Bhatti <dbhatti@sfu.ca>
2016-08-03 22:30:20 +0000

    removing useless schema stuff

--- db/schema.rb
+++ db/schema.rb
@@ -13,15 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20160801211823) do
 
-  create_table "average_caches", force: :cascade do |t|
-    t.integer  "rater_id"
-    t.integer  "rateable_id"
-    t.string   "rateable_type"
-    t.float    "avg",           null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "bath_images", force: :cascade do |t|
     t.integer  "bath_id"
     t.integer  "user_id"
@@ -65,47 +56,6 @@ ActiveRecord::Schema.define(version: 20160801211823) do
   add_index "flags", ["user_id", "review_id"], name: "index_flags_on_user_id_and_review_id", unique: true
   add_index "flags", ["user_id"], name: "index_flags_on_user_id"
 
-  create_table "overall_averages", force: :cascade do |t|
-    t.integer  "rateable_id"
-    t.string   "rateable_type"
-    t.float    "overall_avg",   null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "rates", force: :cascade do |t|
-    t.integer  "rater_id"
-    t.integer  "rateable_id"
-    t.string   "rateable_type"
-    t.float    "stars",         null: false
-    t.string   "dimension"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "rates", ["rateable_id", "rateable_type"], name: "index_rates_on_rateable_id_and_rateable_type"
-  add_index "rates", ["rater_id"], name: "index_rates_on_rater_id"
-
-  create_table "rating_caches", force: :cascade do |t|
-    t.integer  "cacheable_id"
-    t.string   "cacheable_type"
-    t.float    "avg",            null: false
-    t.integer  "qty",            null: false
-    t.string   "dimension"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "rating_caches", ["cacheable_id", "cacheable_type"], name: "index_rating_caches_on_cacheable_id_and_cacheable_type"
-
-  create_table "reviews", force: :cascade do |t|
-    t.text     "post"
-    t.integer  "user_id"
-    t.integer  "bath_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "flag_count"
-  end
 
   add_index "reviews", ["user_id", "bath_id", "created_at"], name: "index_reviews_on_user_id_and_bath_id_and_created_at"
 
